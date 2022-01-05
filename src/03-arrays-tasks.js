019007f3c5ccc7c41ff9ac8a66fbdbb9cf530f72
Aleksey Vasiliyev <alekseyvy@gmail.com>
2022-01-05 13:27:02 +0300

    feat: getMovingSum completed

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -238,8 +238,13 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const calc = [];
+  arr.reduce((acc, val) => {
+    calc.push(acc + val);
+    return acc + val;
+  }, 0);
+  return calc;
 }
 
 /**
