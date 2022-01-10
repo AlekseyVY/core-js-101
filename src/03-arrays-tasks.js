a3523371b35711a86cf1e75e2b989415267a1557
Aleksey Vasiliyev <alekseyvy@gmail.com>
2022-01-10 17:47:26 +0300

    feat: sortCitiesArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -479,8 +479,24 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  arr.sort((a, b) => {
+    if (a.city < b.city) {
+      return -1;
+    } if (a.city > b.city) {
+      return 1;
+    }
+    return 0;
+  });
+  arr.sort((a, b) => {
+    if (a.country < b.country) {
+      return -1;
+    } if (a.country > b.country) {
+      return 1;
+    }
+    return 0;
+  });
+  return arr;
 }
 
 /**
