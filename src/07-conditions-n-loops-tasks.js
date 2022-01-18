d05e0a693ec8935925134c0700ac41a17b7ee06a
AlekseyVY <alekseyvy@gmail.com>
2022-01-18 11:19:32 +0300

    feat: getDigitalRoot

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -306,8 +306,13 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let calculated = num;
+  while (calculated > 9) {
+    const data = String(calculated).split('');
+    calculated = data.reduce((acc, val) => acc + Number(val), 0);
+  }
+  return calculated;
 }
 
 
