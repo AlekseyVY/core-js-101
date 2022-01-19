18eae453905c4db278013b0f618a73f3b211d4c3
AlekseyVY <alekseyvy@gmail.com>
2022-01-19 11:22:47 +0300

    feat: getIdGeneratorFunction

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -194,8 +194,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let self = startFrom - 1;
+  return () => {
+    self += 1;
+    return self;
+  };
 }
 
 
